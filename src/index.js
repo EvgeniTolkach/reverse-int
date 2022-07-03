dcee0a6f9df8310ba3d2f1223cec9d49131fae2e
Evgeni <mg4rulez@gmail.com>
2022-07-04 01:32:55 +0300

    feat: index.js solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const n1 = String(n);
+    const nReverse = n1.split('').map((index) => index === '-' ? '' : index).reverse().join(''); 
+    return nReverse;
 }
